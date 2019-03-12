12e5e1f21584cd67b6c3fe0a0e7f35bfb4bc2f1f
v-susu <v-susu@microsoft.com>
2019-03-12 17:51:42 +0800

    first commit

---
+++ ParallelTestCases.One/TwoTesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParallelTestCases.One
+{
+    [TestClass]
+    public class TwoTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new Common.TestCaseScenario().DoSomething(string.Format("{0}.{1}", this.GetType().FullName, new System.Diagnostics.StackTrace().GetFrame(0).GetMethod().Name), 20);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            new Common.TestCaseScenario().DoSomething(string.Format("{0}.{1}", this.GetType().FullName, new System.Diagnostics.StackTrace().GetFrame(0).GetMethod().Name), 20);
+        }
+    }
+}
---
+++ ParallelTestCases.Two/OneTesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParallelTestCases.Two
+{
+    [TestClass]
+    public class OneTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new Common.TestCaseScenario().DoSomething(string.Format("{0}.{1}", this.GetType().FullName, new System.Diagnostics.StackTrace().GetFrame(0).GetMethod().Name), 20);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            new Common.TestCaseScenario().DoSomething(string.Format("{0}.{1}", this.GetType().FullName, new System.Diagnostics.StackTrace().GetFrame(0).GetMethod().Name), 20);
+        }
+    }
+}
