fe635e776820a7c862edeee3131d967906f8da4b
sunqixinxin <sunqixinxin@126.com>
2019-03-13 09:22:06 +0800

    update

--- ParallelTestCases.Common/TestCaseScenario.cs
+++ ParallelTestCases.Common/TestCaseScenario.cs
@@ -8,12 +8,11 @@ namespace ParallelTestCases.Common
 {
     public class TestCaseScenario
     {
-        public void DoSomething(string message, int runSenconds = 1)
+        public void DoSomething(string message, int repeatTimes = 1)
         {
             Console.WriteLine(string.Format("[{0:yyyy-MM-dd HH:mm:ss:fff}] Start run {1}", DateTime.Now, message));
 
-            int count = runSenconds * 2;
-            for (int i = 0; i < runSenconds; i++)
+            for (int i = 0; i < repeatTimes; i++)
             {
                 Console.WriteLine(string.Format("[{0:yyyy-MM-dd HH:mm:ss:fff}] Sleeping {1}", DateTime.Now, message));
                 System.Threading.Thread.Sleep(new Random().Next(400, 600));
